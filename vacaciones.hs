f630689ae59b155bae787b26320f1c69b251e49e
Nicolas <naguero@frba.utn.edu.ar>
2024-05-29 17:34:52 -0300

    terminado hasta punto 3

--- vacaciones.hs
+++ vacaciones.hs
@@ -1,3 +1,4 @@
+
 data Turista = Turista {
 nivelDeStress :: Int,
 nivelDeCansancio :: Int,
@@ -22,7 +23,7 @@ nivelDeStress = 15,
 nivelDeCansancio = 15,
 conAcompanante = True,
 idiomas = ["aleman"] 
-
+}
 cathi = Turista {
 nivelDeStress = 15,
 nivelDeCansancio = 15,
@@ -30,7 +31,7 @@ conAcompanante = True,
 idiomas = ["aleman", "catalan"]
 
 }
-}
+
 
 
 -- PUNTO 1
@@ -65,10 +66,10 @@ hacerCaminata minutos = (mapCansancio (+ (intensidadCaminata minutos)) . mapStre
 intensidadCaminata :: Int -> Int 
 intensidadCaminata minutos = div minutos 4  
 
-data Marea =  Fuerte | Moderada | Tranquila (deriving Eq)
+data Marea =  Fuerte | Moderada | Tranquila deriving (Eq)
 
-estadoDeMarea :: Marea -> Turista -> Turista
-estadoDeMarea unaMarea unTurista
+paseoEnBarco :: Marea -> Turista -> Turista
+paseoEnBarco unaMarea unTurista
     | unaMarea == Fuerte = (mapCansancio (+ 10). mapStress (+6)) unTurista
     | unaMarea == Tranquila = (hablarIdiomas "aleman".apreciarPaisaje "mar".hacerCaminata 10 ) unTurista 
     | otherwise = unTurista
@@ -81,10 +82,10 @@ type Excursion = Turista -> Turista
 
 
 hacerExcursion :: Excursion -> Turista -> Turista
-hacerExcursion unaExcursion unTurista = (mapStress (subtract porcentageDeStress 10).unaExcursion) $ unTurista
+hacerExcursion unaExcursion unTurista = (mapStress (subtract (porcentageDeStress 10 unTurista)) . unaExcursion)  unTurista
 
-porcentageDeStress :: Turista -> Int -> Int 
-porcentageDeStress porcentage = div ( porcentage * nivelDeStress) 100  
+porcentageDeStress ::  Int -> Turista  -> Int 
+porcentageDeStress  porcentage unTurista  = div ( porcentage * nivelDeStress unTurista) 100  
 
 
 -- PUNTO 2B 
@@ -92,5 +93,51 @@ porcentageDeStress porcentage = div ( porcentage * nivelDeStress) 100
 deltaSegun :: (a -> Int) -> a -> a -> Int
 deltaSegun f algo1 algo2 = f algo1 - f algo2
 
-deltaExcursionSegun :: (a -> Int) -> a -> Excursion -> Int
+deltaExcursionSegun :: (Turista -> Int) -> Turista -> Excursion -> Int
 deltaExcursionSegun indice unTurista unaExcursion = deltaSegun indice unTurista (hacerExcursion unaExcursion unTurista)
+
+--C
+
+educativa :: Excursion -> Turista -> Bool 
+educativa unaExcursion unTurista = ((== 1).deltaExcursionSegun (length.idiomas) unTurista )unaExcursion 
+
+desestresantes :: Excursion -> Turista -> Bool 
+desestresantes unaExcursion unTurista = ((<=3).deltaExcursionSegun nivelDeStress unTurista ) unaExcursion
+
+type Tour = [Excursion]
+
+
+completo :: Tour 
+completo = [hacerCaminata 20, apreciarPaisaje "cascada", hacerCaminata 40, hablarIdiomas "melmacquiano"]
+
+ladoB :: Excursion -> Tour 
+ladoB unaExcursion = [paseoEnBarco Tranquila, unaExcursion, hacerCaminata 120]
+
+islaVecina :: Excursion -> Marea -> Tour 
+islaVecina unaExcursion unaMarea
+    | unaMarea == Fuerte = [paseoEnBarco Fuerte, irAlaPlaya, apreciarPaisaje "lago", paseoEnBarco Fuerte]
+    | otherwise = [paseoEnBarco unaMarea, irAlaPlaya, unaExcursion, paseoEnBarco unaMarea]
+
+--PUNTO A
+
+hacerTour :: Tour -> Turista -> Turista
+hacerTour unTour unTurista = foldl (\x f -> f x) (mapStress (+ length unTour) unTurista) unTour
+
+
+
+analizarTour :: Tour -> Turista -> Bool
+analizarTour unTour unTurista = any (convincente unTurista) unTour 
+
+convincente :: Turista -> Excursion -> Bool 
+convincente unTurista unaExcursion = desestresantes unaExcursion unTurista && conAcompanante(hacerExcursion unaExcursion unTurista)  
+
+ 
+
+espiritualidad :: Tour -> Turista -> Int 
+espiritualidad unTour unTurista = (deltaSegun nivelDeStress (hacerTour unTour unTurista) unTurista)  + (deltaSegun nivelDeCansancio (hacerTour unTour unTurista) unTurista)
+
+convencidos :: [Turista] -> Tour -> [Turista]  
+convencidos conjuntoTuristas unTour = filter (analizarTour unTour)  conjuntoTuristas
+
+totalEspiritualidad :: [Turista] -> Tour -> Int 
+totalEspiritualidad conjuntoTuristas unTour = (sum . map (espiritualidad unTour) )(convencidos conjuntoTuristas unTour)
